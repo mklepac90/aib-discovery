f979b5fab25bf6415a9246c4d2285acab24a8d29
Michael Klepac <67074804+mklepac90@users.noreply.github.com>
2023-11-23 17:09:54 -0500

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,38 @@
-import { ReactNode } from 'react'
-import './globals.css'
+import { ReactNode } from "react";
+import "./globals.css";
+
+import { classNames } from "@/utils/helpers";
+import { Inter } from "next/font/google";
+import Nav from "@/components/Nav";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'ChatGPT',
-  description: 'A chatbot powered by GPT',
-}
+  title: "ChatGPT",
+  description: "A chatbot powered by GPT",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang="en" className="h-full">
+      <head></head>
+
+      <body
+        className={classNames(
+          "h-full bg-white dark:bg-slate-800 transition duration-300 text-gray-900 dark:text-gray-100",
+          inter.className
+        )}
+      >
+        <header>
+          <Nav />
+        </header>
+
+        <main>
+          <div className="min-h-screen py-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
-  )
+  );
 }
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,18 +1,15 @@
 "use client";
-import type OpenAI from "openai";
 import { useEffect, useRef, useState } from "react";
 
-const Form = ({ modelsList }: { modelsList: OpenAI.ModelsPage }) => {
+const Form = () => {
   const messageInput = useRef<HTMLTextAreaElement | null>(null);
-  // causes rerender without useEffect due to suspense boundary
-  // const storedResponse = typeof localStorage !== 'undefined' ? localStorage.getItem('response') : null;
-  // const initialHistory = storedResponse ? JSON.parse(storedResponse) : [];
-  // const [history, setHistory] = useState<string[]>(initialHistory);
   const [history, setHistory] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  // const [models, setModels] = useState<ModelType[]>([])
-  const [models, setModels] = useState(modelsList.data);
-  const [currentModel, setCurrentModel] = useState<string>("dall-e-3");
+
+  const reset = () => {
+    localStorage.setItem("response", JSON.stringify([]));
+    setHistory([]);
+  };
 
   const handleEnter = (
     e: React.KeyboardEvent<HTMLTextAreaElement> &
@@ -44,10 +41,8 @@ const Form = ({ modelsList }: { modelsList: OpenAI.ModelsPage }) => {
       },
       body: JSON.stringify({
         message,
-        currentModel,
       }),
     });
-    console.log("Edge function returned.");
 
     if (!response.ok) {
       throw new Error(response.statusText);
@@ -57,7 +52,6 @@ const Form = ({ modelsList }: { modelsList: OpenAI.ModelsPage }) => {
     if (!data) {
       return;
     }
-    console.log("data in FE", data);
 
     const reader = data.getReader();
     const decoder = new TextDecoder();
@@ -70,119 +64,112 @@ const Form = ({ modelsList }: { modelsList: OpenAI.ModelsPage }) => {
       const { value, done: doneReading } = await reader.read();
       done = doneReading;
       const chunkValue = decoder.decode(value);
-      // currentResponse = [...currentResponse, message, chunkValue];
       currentResponse = [...currentResponse, chunkValue];
       setHistory((prev) => [...prev.slice(0, -1), currentResponse.join("")]);
-      console.log("rerender");
     }
-    console.log("rerender-2");
-    // breaks text indent on refresh due to streaming
-    // localStorage.setItem('response', JSON.stringify(history))
-    setIsLoading(false);
-  };
 
-  const handleReset = () => {
-    localStorage.removeItem("response");
-    setHistory([]);
+    setIsLoading(false);
   };
 
   // Save the 'history' state to 'localStorage' whenever it changes
   useEffect(() => {
-    localStorage.setItem("response", JSON.stringify(history));
+    if (history.length) {
+      localStorage.setItem("response", JSON.stringify(history));
+    }
   }, [history]);
 
   // Initialize 'history' state from 'localStorage' when the component mounts
   useEffect(() => {
     const storedResponse = localStorage.getItem("response");
+
     if (storedResponse) {
       setHistory(JSON.parse(storedResponse));
     }
   }, []);
 
-  const handleModelChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setCurrentModel(e.target.value);
-  };
-
   return (
-    <div className="flex justify-center">
-      <select
-        value={currentModel}
-        onChange={handleModelChange}
-        className="w-72 fixed top-5 left-5 outline-none border-none p-4 rounded-md bg-white text-gray-500 dark:hover:text-gray-400 dark:hover:bg-gray-900"
-      >
-        {models.map((model) => (
-          <option key={model.id} value={model.id}>
-            {model.id}
-          </option>
-        ))}
-      </select>
-
-      <button
-        onClick={handleReset}
-        type="reset"
-        className="fixed top-5 right-5 p-4 rounded-md bg-white text-gray-500 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent"
-      >
-        Clear History
-      </button>
-      <div className="w-full mx-2 flex flex-col items-start gap-3 pt-6 last:mb-6 md:mx-auto md:max-w-3xl">
-        {isLoading
-          ? history.map((item: any, index: number) => {
-              return (
-                <div
-                  key={index}
-                  className={`${
-                    index % 2 === 0 ? "bg-blue-500" : "bg-gray-500"
-                  } p-3 rounded-lg`}
-                >
-                  <p>{item}</p>
-                </div>
-              );
-            })
-          : history
-          ? history.map((item: string, index: number) => {
-              return (
-                <div
-                  key={index}
-                  className={`${
-                    index % 2 === 0 ? "bg-blue-500" : "bg-gray-500"
-                  } p-3 rounded-lg`}
-                >
-                  <p>{item}</p>
-                </div>
-              );
-            })
-          : null}
+    <div>
+      <div className="w-full justify-center sm:justify-end flex mb-10">
+        <div className="bg-gray-900 shadow-md rounded-lg text-gray-100 dark:text-gray-900 dark:bg-gray-100 w-48 text-center p-2 text-xl">
+          2:38 Remaining
+        </div>
       </div>
-      <form
-        onSubmit={handleSubmit}
-        className="fixed bottom-0 w-full md:max-w-3xl bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] mb-4"
-      >
-        <textarea
-          name="Message"
-          placeholder="Type your query"
-          ref={messageInput}
-          onKeyDown={handleEnter}
-          className="w-full resize-none bg-transparent outline-none pt-4 pl-4 translate-y-1"
-        />
-        <button
-          disabled={isLoading}
-          type="submit"
-          className="absolute top-[1.4rem] right-5 p-1 rounded-md text-gray-500 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent"
+
+      <div className="mb-10 text-center">
+        <h2 className="text-2xl font-bold leading-7 sm:truncate sm:text-4xl sm:tracking-tight mb-2">
+          Challenge
+        </h2>
+
+        <p className="text-lg w-full sm:text-2xl sm:w-4/6 mx-auto">
+          Design a financial solution that enhances both personal banking
+          experiences and addresses future challenges in the banking industry.
+        </p>
+      </div>
+
+      <button onClick={reset}>Reset</button>
+
+      <div className="h-[48rem] p-2 sm:p-5 bg-gray-50 dark:bg-slate-900 rounded-md border shadow transition duration-300">
+        <div className="h-5/6 overflow-auto prose lg:prose-lg dark:prose-invert max-w-none">
+          {isLoading
+            ? history.map((item: any, index: number) => {
+                return (
+                  <div
+                    key={index}
+                    className={`${
+                      index % 2 === 0 ? "font-medium" : "indent-8"
+                    }`}
+                  >
+                    <p>{item}</p>
+                  </div>
+                );
+              })
+            : history
+            ? history.map((item: string, index: number) => {
+                return (
+                  <div
+                    key={index}
+                    className={`${
+                      index % 2 === 0 ? "font-medium" : "indent-8"
+                    }`}
+                  >
+                    <p>{item}</p>
+                  </div>
+                );
+              })
+            : null}
+        </div>
+
+        <form
+          onSubmit={handleSubmit}
+          className="w-full mt-1 mx-auto h-1/6 bg-white dark:bg-black rounded-md border flex p-2"
         >
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth="0"
-            viewBox="0 0 20 20"
-            className="h-4 w-4 rotate-90"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
+          <textarea
+            name="Message"
+            placeholder="Enter prompt"
+            ref={messageInput}
+            onKeyDown={handleEnter}
+            className="text-lg w-full resize-none bg-transparent outline-none"
+          />
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="p-1 rounded-md text-gray-500 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent"
           >
-            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-          </svg>
-        </button>
-      </form>
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 20 20"
+              className="h-6 w-6 rotate-90"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+            </svg>
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
